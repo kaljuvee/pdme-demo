e68fb14199aa5e416462bc059892ee7ff3e64291
Julian Kaljuvee <kaljuvee@gmail.com>
2024-08-01 05:48:55 +0300

    elo interactive

--- tests/elo_test.py
+++ tests/elo_test.py
@@ -1,12 +1,17 @@
 import pandas as pd
 from pdme.evaluate import pdme_llm
+from utils import calculate_elo_iterative
 
-battles = pd.read_csv('data/generic_battles.csv')
+battles = pd.read_csv('data/generic_battle-20240726-0900.csv')
+print(battles.head())
+#alibration_model = 'claude-3-opus-20240229'
 
-calibration_model = 'claude-3-opus-20240229'
-
-elo_df = pdme_llm.compute_online_elo(battles, calibration_model)
+#elo_df = pdme_llm.compute_online_elo(battles, calibration_model)
 # Convert to DataFrame
 
-elo_df.to_csv('data/pdme_elo_generic.csv', index=False)
-print(elo_df.head())
+#elo_df.to_csv('data/pdme_elo_generic.csv', index=False)
+
+elo_iter_df = calculate_elo_iterative(battles)
+elo_iter_df.to_csv('data/pdme_elo_generic_iterative.csv', index=False)
+
+print(elo_iter_df.head())
